152bc348858c8cc14d8c090c39660a6a644d4d82
MikeSanDev <mjs070594@gmail.com>
2024-05-30 09:04:27 -0500

    first test

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { useRouter } from "next/router";
+
+const Navbar = () => {
+  return (
+    <>
+    <h1 className="text-white">Navbar</h1>
+    </>
+  );
+};
+
+export default Navbar;
