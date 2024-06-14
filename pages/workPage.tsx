41b3aec0eb63e582bb4cdbb95d978c7e5b31b6c0
MikeSanDev <mjs070594@gmail.com>
2024-06-14 10:06:43 -0500

    updated changes with Aaron

---
+++ pages/workPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import Work from "@/components/Work";
+
+
+export default function workPage() {
+    return (
+        <div>
+            <Navbar />
+            <Work />
+        </div>
+    );
+}
