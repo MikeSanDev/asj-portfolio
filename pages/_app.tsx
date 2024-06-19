8de2a5716482ab69eb123ca6bb9abcde45ff675f
MikeSanDev <mjs070594@gmail.com>
2024-06-19 10:34:40 -0500

    build fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,9 @@
 import '../styles/globals.css'; // Ensure this path is correct
 import 'tailwindcss/tailwind.css';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
+
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
