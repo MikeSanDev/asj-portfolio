5e25697513f05ae9fbdc7b12da463d56646fb9e0
MikeSanDev <mjs070594@gmail.com>
2024-08-20 14:09:48 -0500

    home page animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
 "use client";
-
+import { motion } from 'framer-motion';
 import Image from "next/image";
 import Link from 'next/link';
 import { useState } from 'react';
 import "../styles/globals.css";
 
-
 export default function Home() {
+  const fadeInBackground = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 2, // 1-second duration for the background image
+      },
+    },
+  };
+
+  const fadeInBalls = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 4, // 3-second duration for the balls
+      },
+    },
+  };
+
   const [message, setMessage] = useState("");
 
   const handleBallClick = (route: string) => {
@@ -23,7 +42,12 @@ export default function Home() {
 
   return (
     <div>
-      <div className="relative w-full h-screen pkmn-page">
+      <motion.div
+        className="relative w-full h-screen pkmn-page"
+        initial="hidden"
+        animate="visible"
+        variants={fadeInBackground}
+      >
         <Image
           src="/assets/pkmn-bg.png"
           alt="background"
@@ -35,11 +59,14 @@ export default function Home() {
           <li className="absolute no-bullets ball1" style={{ top: '30%', left: '15%' }} onClick={() => handleBallClick('/workPage')}
             onMouseEnter={() => handleMouseEnter('Work Experience')}
             onMouseLeave={handleMouseLeave}>
-            <Image
+            <motion.img
               src="/assets/ball1.png"
               alt="ball1"
               width={150}
               height={100}
+              initial="hidden"
+              animate="visible"
+              variants={fadeInBalls}
             />
           </li>
         </Link>
@@ -47,11 +74,14 @@ export default function Home() {
           <li className="absolute no-bullets ball2" style={{ top: '30%', right: '17%' }} onClick={() => handleBallClick('/aboutPage')}
             onMouseEnter={() => handleMouseEnter('About Me')}
             onMouseLeave={handleMouseLeave}>
-            <Image
+            <motion.img
               src="/assets/ball2.png"
               alt="ball2"
               width={150}
               height={100}
+              initial="hidden"
+              animate="visible"
+              variants={fadeInBalls}
             />
           </li>
         </Link>
@@ -59,15 +89,18 @@ export default function Home() {
           <a target="_blank" rel="noopener noreferrer" className="absolute no-bullets ball3" style={{ bottom: '20%', left: '44.3%' }} onClick={() => handleBallClick('https://drive.google.com/file/d/1XN3fCDg3PXbmL4PLIJiW_nkYljsL2e9j/view')}
             onMouseEnter={() => handleMouseEnter('Resume')}
             onMouseLeave={handleMouseLeave}>
-            <Image
+            <motion.img
               src="/assets/ball3.png"
               alt="ball3"
               width={150}
               height={100}
+              initial="hidden"
+              animate="visible"
+              variants={fadeInBalls}
             />
           </a>
         </Link>
-      </div>
+      </motion.div>
       <div className={`pkmn-msg ${message ? 'opacity-100' : 'opacity-0'} transition-opacity fixed bottom-0 left-0 right-1 mx-auto p-4 mb-4`}>
         {message}
       </div>
