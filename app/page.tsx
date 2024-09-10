93cf20b813d9268197d38cef474f0930264cc633
MikeSanDev <mjs070594@gmail.com>
2024-09-10 08:58:32 -0500

    first two sections complete

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 import "../styles/globals.css";
 import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   // Animations
@@ -43,63 +44,142 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <motion.div
-        className="relative w-full h-screen pkmn-page"
-        initial="hidden"
-        animate="visible"
-        variants={fadeInBackground}
-      >
-        <Navbar />
-        {/* POKEBALLS - SAVE FOR SECTION 2 OF THIS PAGE */}
-        {/* 
-        <Link href="/workPage">
-          <li className="absolute no-bullets ball1" style={{ top: '30%', left: '15%' }} onClick={() => handleBallClick('/workPage')}
-            onMouseEnter={() => handleMouseEnter('Work Experience')}
-            onMouseLeave={handleMouseLeave}>
-            <motion.img
-              src="/assets/ball1.png"
-              alt="ball1"
-              width={150}
-              height={100}
-              initial="hidden"
-              animate="visible"
-              variants={fadeInBalls}
-            />
-          </li>
-        </Link>
-        <Link href="/aboutPage">
-          <li className="absolute no-bullets ball2" style={{ top: '30%', right: '17%' }} onClick={() => handleBallClick('/aboutPage')}
-            onMouseEnter={() => handleMouseEnter('About Me')}
-            onMouseLeave={handleMouseLeave}>
-            <motion.img
-              src="/assets/ball2.png"
-              alt="ball2"
-              width={150}
-              height={100}
-              initial="hidden"
-              animate="visible"
-              variants={fadeInBalls}
-            />
-          </li>
-        </Link>
-        <Link href="/assets/ASJ-resume.pdf" legacyBehavior>
-          <a target="_blank" rel="noopener noreferrer" className="absolute no-bullets ball3" style={{ bottom: '20%', left: '44.3%' }} onClick={() => handleBallClick('https://drive.google.com/file/d/1XN3fCDg3PXbmL4PLIJiW_nkYljsL2e9j/view')}
-            onMouseEnter={() => handleMouseEnter('Resume')}
-            onMouseLeave={handleMouseLeave}>
-            <motion.img
-              src="/assets/ball3.png"
-              alt="ball3"
-              width={150}
-              height={100}
-              initial="hidden"
-              animate="visible"
-              variants={fadeInBalls}
-            />
-          </a>
-        </Link> */}
-      </motion.div>
 
-    </div>
+    <motion.div
+      className="relative w-full h-screen pkmn-page"
+      initial="hidden"
+      animate="visible"
+      variants={fadeInBackground}
+    >
+      <Navbar />
+      <section id='section1'>
+        {/* SECTION 1 - INTRO */}
+        <div className="asj-width-margin mx-auto px-4 py-20 bg-white">
+          <h1 className="from-neutral-300 text-center text-2xl italic font-extrabold">Hello there! I'm Aaron San Jose A product designer focused on congenial and effective designs</h1>
+
+        </div>
+      </section>
+      {/* SECTION 2 - POKEBALLS */}
+      <section id='section2'>
+        <div className="asj-width-margin mx-auto px-4 py-10" style={{ backgroundColor: '#9647ff' }}>
+          <h1 className="text-black text-center text-2xl italic font-extrabold"
+            style={{ textShadow: '2px 2px 0 #ffffff, -2px 2px 0 #ffffff, 2px -2px 0 #ffffff, -2px -2px 0 #ffffff' }}>
+            The most influential decision I experienced growing up was...
+          </h1>
+
+          <div className="flex justify-center items-center space-x-8 mt-8">
+            <div className="flex flex-col items-center">
+              <Link href="/workPage">
+                <motion.img
+                  src="/assets/ball1.png"
+                  alt="ball1"
+                  width={150}
+                  height={100}
+                  className="cursor-pointer"
+                  initial="hidden"
+                  animate="visible"
+                  variants={fadeInBalls}
+                  whileHover={{ scale: 1.2 }} // Expands on hover
+                  onClick={() => handleBallClick('/workPage')}
+                  onMouseEnter={() => handleMouseEnter('Work Experience')}
+                  onMouseLeave={handleMouseLeave}
+                />
+              </Link>
+              <p className="text-white mt-4">Work</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <Link href="/aboutPage">
+                <motion.img
+                  src="/assets/ball2.png"
+                  alt="ball2"
+                  width={150}
+                  height={100}
+                  className="cursor-pointer"
+                  initial="hidden"
+                  animate="visible"
+                  variants={fadeInBalls}
+                  whileHover={{ scale: 1.2 }} // Expands on hover
+                  onClick={() => handleBallClick('/aboutPage')}
+                  onMouseEnter={() => handleMouseEnter('About Me')}
+                  onMouseLeave={handleMouseLeave}
+                />
+              </Link>
+              <p className="text-white mt-4">About</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <Link href="/assets/ASJ-resume.pdf" legacyBehavior>
+                <a target="_blank" rel="noopener noreferrer">
+                  <motion.img
+                    src="/assets/ball3.png"
+                    alt="ball3"
+                    width={150}
+                    height={100}
+                    className="cursor-pointer"
+                    initial="hidden"
+                    animate="visible"
+                    variants={fadeInBalls}
+                    whileHover={{ scale: 1.2 }} // Expands on hover
+                    onClick={() => handleBallClick('https://drive.google.com/file/d/1XN3fCDg3PXbmL4PLIJiW_nkYljsL2e9j/view')}
+                    onMouseEnter={() => handleMouseEnter('Resume')}
+                    onMouseLeave={handleMouseLeave}
+                  />
+                </a>
+              </Link>
+              <p className="text-white mt-4">Contact</p>
+            </div>
+          </div>
+          <h1 className="text-black text-center text-2xl italic font-extrabold pt-10"
+            style={{ textShadow: '2px 2px 0 #ffffff, -2px 2px 0 #ffffff, 2px -2px 0 #ffffff, -2px -2px 0 #ffffff' }}>
+            Choose your starter
+          </h1>
+        </div>
+      </section>
+
+      {/* SECTION3 - WORK */}
+      <section id='section3'>
+        < motion.div
+          className="asj-width-margin mx-auto px-4 py-10"
+          initial="hidden"
+          animate="visible"
+        >
+          {/* Case Studies */}
+          <h1 className="sub-header text-black pt-5" > Case Studies</h1 >
+          <div className="py-5">
+            <strong className="from-neutral-300">Chicago Scholars</strong>
+            <p className="text-2xl italic font-extralight work-text pb-5">Reach Dashboard - A product that manages student support systems for continuing education staff.</p>
+            <Link href="/reachPage">
+              <Image
+                className="img-border"
+                src="/assets/dashboard.png"
+                alt='Reach Dashboard'
+                width={800}
+                height={400} />
+            </Link>
+          </div>
+          <div className="py-5">
+            <strong className="from-neutral-300">Coding Dojo</strong>
+            <p className="text-2xl italic font-extralight work-text pb-5">Craigslist Moodboard - A redesign of Craigslist encouraging user engagement.</p>
+            <Link href="/craigsList">
+              <Image
+                className="mb-10"
+                src="/assets/craiglist-dash.png"
+                alt='Coding Dojo Craigslist Dashboard'
+                width={800}
+                height={400} />
+            </Link>
+          </div>
+        </motion.div >
+      </section>
+
+      {/* SECTION 4 - ABOUT */}
+      <section id='section4'>
+
+      </section>
+      {/* Footer */}
+      < Footer />
+    </motion.div >
+
   );
 }
