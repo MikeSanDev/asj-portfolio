d3fca3ed52cc550487eb764c6335574eeccb5eb7
MikeSanDev <mjs070594@gmail.com>
2024-09-17 11:36:20 -0500

    creating more space in the sections

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,17 +2,18 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer className="bg-black p-6" >
+        <footer className="bg-black p-10" >
             <div className='text-white flex justify-center flex-col footer-width-margin mx-auto px-4 '>
+                <h1 className='text-2xl ml-4 text-center underline italic'>Connect with me:</h1>
                 <div className='pt-4 flex flex-row justify-center pb-4'>
-                    <Link href="/">
-                        <p className='footer-text underline italic'>Home</p>
+                    <Link href="https://www.instagram.com/ernsanjose/" legacyBehavior>
+                        <a target="_blank" rel="noopener noreferrer" className='ml-4 italic'>Instagram</a>
                     </Link>
                     <Link href="https://www.linkedin.com/in/asanjose/" legacyBehavior>
-                        <a target="_blank" rel="noopener noreferrer" className='ml-4 underline italic'>Linkedin</a>
+                        <a target="_blank" rel="noopener noreferrer" className='ml-4 italic'>Linkedin</a>
                     </Link>
                     <Link href="/assets/ASJ-resume.pdf" legacyBehavior>
-                        <a target="_blank" rel="noopener noreferrer" className='ml-4 underline italic '>Resume</a>
+                        <a target="_blank" rel="noopener noreferrer" className='ml-4 italic '>Resume</a>
                     </Link>
                 </div>
             </div>
