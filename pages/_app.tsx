1e7e603d33b92ca6d43c2a80a137204d8d998b97
MikeSanDev <mjs070594@gmail.com>
2024-10-31 09:00:37 -0500

    favicon and title change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.css'; // Ensure this path is correct
 import 'tailwindcss/tailwind.css';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Aaron San Jose</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
